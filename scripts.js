7a9ac2e85a7c5f3466939955b82456f412094560
Coffroth <morgan.coffroth@wsu.edu>
2022-10-09 22:04:00 +0000

    updated 3 list files

--- scripts.js
+++ scripts.js
@@ -1,6 +1,36 @@
-// Javascript for tp5
+// Javascript for tp6
+
+//function to load file from the URL "frontfile" into the object indetified by "whereto"
+
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML = this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
 
 window.onload = function() {
+  
+    loadFileInto("ingredients.html", "#ingredients ul");
+   loadFileInto("equipment.html", "#equipmequipment ul");
+   loadFileInto("directions.html", "#directions ol");
   
   document.querySelector("#HeadText h1").classList.add("tp");
   
@@ -26,4 +56,7 @@ window.onload = function() {
       
      document.querySelector(".Recipe-Col #ingredients ul").innerHTML += "<li> A hunger for BREAD</li>"
  
+
 } // end window.onload
+
+
